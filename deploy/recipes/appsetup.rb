e68179ab418b06b21aca958cec0805c8c85aceb0
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-11-09 13:30:57 -0500

    adding initial files

---
+++ deploy/recipes/appsetup.rb
@@ -0,0 +1,29 @@
+node[:deploy].each do |app_name, deploy|
+
+  template "#{deploy[:deploy_to]}/current/db-connect.rb" do
+    source "db-connect.rb"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")
+      owner "apache" # Change to nginx/unicorn?
+    end
+
+    variables(
+      host: (deploy[:database][:host] rescue nil),
+      user: (deploy[:database][:username] rescue nil),
+      password: (deploy[:database][:password] rescue nil),
+      db: (deploy[:database][:database] rescue nil),
+      table: (node[:www][:dbtable] rescue nil)
+    )
+
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current")
+    end
+
+  end
+
+end
+      
